56f0573c4652799d94ddc0b492c13eb25f6b9b74
Nerok <90855034+AiorNerok@users.noreply.github.com>
2025-04-21 20:32:26 +0300

    add rename solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,40 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+
+    const __filename = path.dirname(fileURLToPath(import.meta.url));
+
+    const filePath = path.join(__filename, 'files', 'wrongFilename.txt');
+    const fileCopyPath = path.join(__filename, 'files', 'properFilename.md');
+    const err_message = 'FS operation failed';
+
+    console.log(filePath);
+    
+    fs.stat(filePath, (err, stats) => {
+        if (err) {
+            console.error('err_message');
+            return;
+        }
+
+        fs.stat(fileCopyPath, (err, stats) => {
+            if(stats) {
+                console.error(err_message);
+                return;
+            }
+            
+            fs.rename(filePath, fileCopyPath, (err) => {
+                if (err) {
+                    console.error('FS operation failed');
+                    return;
+                }
+            }
+            );
+        })
+    })
+
+
 };
 
 await rename();
