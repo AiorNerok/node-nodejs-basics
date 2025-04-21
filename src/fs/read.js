a791daad7494ef68e110b01720f554476fd751df
Nerok <90855034+AiorNerok@users.noreply.github.com>
2025-04-21 20:57:05 +0300

    add read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__filename, "files/fileToRead.txt");
+    const err_message = 'FS operation failed'
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err_message);
+            return;
+        }
+        console.log(data);
+    });
 };
 
 await read();
