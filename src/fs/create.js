2322c10be125838217bff0f2311f5f54b12dff04
Nerok <90855034+AiorNerok@users.noreply.github.com>
2025-04-21 19:45:04 +0300

    add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const text = 'I am fresh and young';
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    fs.stat(path.join(__dirname, 'files', 'fresh.txt'), (err) => {
+        if (err === null) {
+            console.error('FS operation failed');
+            return;
+        }
+
+        fs.writeFile(path.join(__dirname, 'files', 'fresh.txt'), text, (err) => {
+            if (err) {
+                console.error('Error:', err);
+                return;
+            }
+        });    
+    })    
 };
 
 await create();
