d6eea7c7ef2eee0f80fbb67316f82a43bcd6ee2f
Nerok <90855034+AiorNerok@users.noreply.github.com>
2025-04-21 20:09:39 +0300

    add copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
+import fs from "fs";
+import path,{dirname} from "path";
+import { fileURLToPath } from "url";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = dirname(fileURLToPath(import.meta.url));
+
+  const filePath = path.join(__filename, "files");
+  const fileCopyPath = path.join(__filename, "files_copy");
+
+  const err_message = 'FS operation failed'
+
+    fs.stat(filePath, (err, stats) => {
+        if (err) {
+            console.error(err_message);
+            return;
+        }
+
+        fs.stat(fileCopyPath, (err, stats) => {
+            if (err) {
+                fs.cp(filePath, fileCopyPath, { recursive: true }, (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+                return;
+            }    
+        })   
+
+        console.error(err_message);
+    })  
 };
 
 await copy();
