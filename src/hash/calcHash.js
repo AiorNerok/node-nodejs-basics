77d107b72db8d8accc6b70a44c67b7905df87b27
Nerok <90855034+AiorNerok@users.noreply.github.com>
2025-04-21 13:16:10 +0300

    end hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    
+    const hash = crypto.createHash('sha256');
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error('Error:', err);
+            return;
+        }
+
+        const result = hash.update(data.toString()).digest('hex');
+        console.log(result);
+    });
 };
 
 await calculateHash();
